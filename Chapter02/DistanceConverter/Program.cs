696a78594bb4e1968b2fba44d9bbbebf8c6b6015
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-04-22 09:54:13 +0900

    ファイル追加

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace DistanceConverter {
     internal class Program {
+        //コマンドライン引数で指定された範囲のフィートとメートルの対応表を出力する
         static void Main(string[] args) {
 
             int start = int.Parse(args[1]);
@@ -13,28 +14,28 @@
         }
 
 
-                //フィートからメートルへの対応表を出力
-            static void PrintFeetToMeterList(int start, int end) {
-                for (int feet = start; feet <= end; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine($"{feet}ft = {meter:0.0000}m");
-                }
-            }
-                //メートルからフィートへの対応表を出力
-            static void PrintMeterToFeetList(int start, int end) {
-                for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
-                }
-            }
-
-            static double FeetToMeter(int feet) {
-                return feet * 0.3048;
-            }
-
-            static double MeterToFeet(int meter) {
-                return meter / 0.328084;
-
+        //フィートからメートルへの対応表を出力
+        static void PrintFeetToMeterList(int start, int end) {
+            for (int feet = start; feet <= end; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
         }
+        //メートルからフィートへの対応表を出力
+        static void PrintMeterToFeetList(int start, int end) {
+            for (int meter = 1; meter <= 10; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
+        }
+
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        static double MeterToFeet(int meter) {
+            return meter / 0.328084;
+
+        }
     }
+}
