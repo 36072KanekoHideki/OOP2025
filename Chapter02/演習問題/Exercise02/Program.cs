e8dc98811614bc82d9f7b43a22f19a21cedf7e60
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-04-23 10:16:51 +0900

    インチからメートル変換完成

---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using DistanceConverter;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            PrintInchToMeterList(1, 10);
+
+            //インチからメートルへの対応表を出力
+            static void PrintInchToMeterList(int start, int end) {
+                for (int Inch = start; Inch <= end; Inch++) {
+                    double meter = InchConverter.ToMeter(Inch);
+                    Console.WriteLine($"{Inch}inch = {meter:0.0000}m");
+
+                }
+            }
+        }
+    }
+}
