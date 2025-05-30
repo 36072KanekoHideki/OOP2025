77729847e93223d8802f76b4ef7579a1306de64f
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-05-30 14:59:07 +0900

    6.3.1 完成

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var target = "C# Programming";
+            var isExist = target.All(c => Char.IsLower(c));
+        }
+    }
+}
