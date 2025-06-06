3768d64b5eec40c58a335ddff78bd1b45902e1ba
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-06-06 15:22:51 +0900

    5.1.1

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int year { get; init; }
+        public int Month { get; init; } 
+
+        //5.1.2
+        //設定されている西暦が２１世紀か判定する
+        //Yearが2001
+
+        //5.1.3
+        public YearMonth AddOneMonth() {
+
+        }
+
+        //5.1.4
+        public override string ToString() => 
+
+
+
+    }
+}
