6eee1708fa766c63463cd0fa779a560179551e43
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-06-17 11:46:32 +0900

    設問1_提出

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -4,26 +4,36 @@
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
+            _score = ReadScore(filePath);
         }
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
+            var score = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+                Student scores = new Student() {
+                    Name = items[0],
+                    Subjet = items[1],
+                    Score = int.Parse(items[2])
+                };
+                score.Add(scores);
+            }
+            return score;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-            
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Name)) {
+                    dict[score.Subjet] += score.Score;
+                } else {
+                    dict[score.Subjet] = score.Score;
+                }
+            }
+            return dict;
         }
     }
 }
