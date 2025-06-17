8a5cfefd87960a7780c4412d6d1abaf4154050c7
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-06-17 10:45:04 +0900

    8.2

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -8,6 +8,9 @@
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
 
+            //8.2.3 (Countの呼び出し例)
+            //上のAddメソッドで、2つのオブジェクトを追加しているので、読み込んだ
+
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
             foreach (var name in names) {
@@ -20,6 +23,10 @@
             }
             Console.WriteLine();
 
+
+
+            //8.2.4
+            var query = abbrs.GetAll().Where(x => x.Key.Length == 3);
             // ToAbbreviationメソッドの利用例
             var japanese = "東南アジア諸国連合";
             var abbreviation = abbrs.ToAbbreviation(japanese);
