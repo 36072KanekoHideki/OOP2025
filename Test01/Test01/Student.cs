63cdcf811485565cd3ac5e5928606e65f1843b07
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-06-17 11:14:52 +0900

    設問1_Student クラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; set; } = String.Empty;
+
+        public string Subjet { get; set; } = String.Empty;
+
+        public int Score { get; set; } 
+
+    }
+}
