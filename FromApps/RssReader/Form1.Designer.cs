dd4456c67d6eb73b2f16f4a4fe55332855641cf2
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-07-18 14:28:07 +0900

    RssReader タイトル選択

---
+++ FromApps/RssReader/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace RssReader {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            btRssGet = new Button();
+            lbTitles = new ListBox();
+            tbUrl = new TextBox();
+            wvRssLink = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)wvRssLink).BeginInit();
+            SuspendLayout();
+            // 
+            // btRssGet
+            // 
+            btRssGet.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btRssGet.Location = new Point(424, 20);
+            btRssGet.Name = "btRssGet";
+            btRssGet.Size = new Size(74, 31);
+            btRssGet.TabIndex = 1;
+            btRssGet.Text = "取得";
+            btRssGet.UseVisualStyleBackColor = true;
+            btRssGet.Click += btRssGet_Click;
+            // 
+            // lbTitles
+            // 
+            lbTitles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lbTitles.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            lbTitles.FormattingEnabled = true;
+            lbTitles.ItemHeight = 21;
+            lbTitles.Location = new Point(32, 118);
+            lbTitles.Name = "lbTitles";
+            lbTitles.Size = new Size(1039, 151);
+            lbTitles.TabIndex = 2;
+            lbTitles.Click += lbTitles_Click;
+            // 
+            // tbUrl
+            // 
+            tbUrl.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            tbUrl.Location = new Point(32, 20);
+            tbUrl.Multiline = true;
+            tbUrl.Name = "tbUrl";
+            tbUrl.Size = new Size(386, 31);
+            tbUrl.TabIndex = 3;
+            // 
+            // webView21
+            // 
+            wvRssLink.AllowExternalDrop = true;
+            wvRssLink.CreationProperties = null;
+            wvRssLink.DefaultBackgroundColor = Color.White;
+            wvRssLink.Location = new Point(32, 275);
+            wvRssLink.Name = "webView21";
+            wvRssLink.Size = new Size(1039, 438);
+            wvRssLink.TabIndex = 4;
+            wvRssLink.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1095, 752);
+            Controls.Add(wvRssLink);
+            Controls.Add(tbUrl);
+            Controls.Add(lbTitles);
+            Controls.Add(btRssGet);
+            Name = "Form1";
+            Text = "RSSリーダー";
+            ((System.ComponentModel.ISupportInitialize)wvRssLink).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button btRssGet;
+        private ListBox lbTitles;
+        private TextBox tbUrl;
+        private Microsoft.Web.WebView2.WinForms.WebView2 wvRssLink;
+    }
+}
