601a6e7c0bcce0a0f1af20fdd0e394b277363e00
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-09-02 15:59:58 +0900

    UI レイアウト

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -18,15 +18,9 @@ namespace SampleApplication
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            MessageBox.Show("群馬県太田市");
         }
     }
 }
