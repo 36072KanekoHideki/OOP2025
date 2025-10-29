8ad658587c327e408809ea55e74b3e8685a881aa
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-10-29 12:10:22 +0900

    Exercise1_7

---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var groups = Library.Categories
+                            .GroupJoin(Library.Books
+                                    , c => c.Id
+                                    , b => b.CategoryId,
+                                    (c, books) => new {
+                                        Category = c.Name,
+                                        Books = books,
+                                    });
+
+            foreach (var book in books) {
+                Console.WriteLine($"{book!.Title}年 {book!.category} ({book!.PublishedYear})");
+            }
+        }
+    }
+}
