169d915d2f6f7219ad0c31131bc8af21bb0e3221
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-11-05 11:17:06 +0900

    コード14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient(); 
+            await GetHtmlExample(hc);
+        }
+
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://ja.wikipedia.org/wiki/%E3%82%A6%E3%82%A3%E3%82%AD";
+            var text = await httpClient.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
