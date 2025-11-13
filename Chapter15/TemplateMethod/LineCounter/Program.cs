09a2fe0e90595fcafe33b64206473eb8d289e11e
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-11-13 15:56:14 +0900

    strategyパターン

--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -3,7 +3,8 @@
 namespace LineCounter {
     internal class Program {
         static void Main(string[] args) {
-            TextProcessor.Run<LineCounterProcessor>(args[0]);
+            string path = @"C:\Users\infosys\source\repos\OOP2025\Chapter12\Exercise03\Program.cs";
+            TextProcessor.Run<LineCounterProcessor(Path);
         }
     }
 }
