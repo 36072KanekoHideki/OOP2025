85da7a66dfae7c10dfd401beaafb381c4fc80cf7
ヒデキ 金子 <oibc36072@ojs.ac.jp>
2025-11-17 15:06:08 +0900

    依存性の注入パターン

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextFileProcessorDI {
+    internal class Program {
+        static void Main(string[] args) {
+            var service = new LineCounterService();
+            var Processor = new TextFileProcessor(service);
+            Console.WriteLine("パスの入力：");
+            Processor.Run(Console.ReadLine());  
+        }
+    }
+}
---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/TextFileProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class TextFileProcessor {
+        private ITextFilrService _service;
+
+        //コンストラクター
+        public TextFileProcessor(ITextFilrService service) {
+            _service = service;
+        }
+
+        public void Run(string fileName) {
+            _service.Initialize(fileName);
+
+            var lines = File.ReadLines(fileName);
+            foreach (var line in lines) {
+                _service.Execute(line);
+            }
+            _service.Terminate();
+        }
+    }
+}
